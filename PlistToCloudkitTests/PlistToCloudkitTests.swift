b60e3ed1f70a2072fe70ed48a230b5354bb93583
dawand <dwnjls@gmail.com>
2016-03-31 18:54:27 +0100

    Initial Commit

---
+++ PlistToCloudkitTests/PlistToCloudkitTests.swift
@@ -0,0 +1,36 @@
+//
+//  PlistToCloudkitTests.swift
+//  PlistToCloudkitTests
+//
+//  Created by Dawand Sulaiman on 31/03/2016.
+//  Copyright © 2016 carrotApps. All rights reserved.
+//
+
+import XCTest
+@testable import PlistToCloudkit
+
+class PlistToCloudkitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
